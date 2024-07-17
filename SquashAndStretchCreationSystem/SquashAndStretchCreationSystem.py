99a50f7f271b07810b4b0dd6f416175f345a7133
AudreyPara <AudreyParansky@gmail.com>
2024-07-17 04:10:22 -0700

    Adding modularity to stretchy joints on existing joint chain
    still needs to fix cycles and scaling for in between joints

--- SquashAndStretchCreationSystem/SquashAndStretchCreationSystem.py
+++ SquashAndStretchCreationSystem/SquashAndStretchCreationSystem.py
@@ -24,21 +24,21 @@ def create_ui(pWindowTitle):
     cmds.rowLayout( adj=2, numberOfColumns=3 )
     cmds.text( label='first' )
     originParentField = cmds.textField( pht='Origin Parent Joint', ed=True )
-    cmds.button( label='Select', command=functools.partial( get_selection, originParentField ) )
+    cmds.button( label='Select', command=functools.partial( get_selection, originParentField, 'joint' ) )
 
     cmds.setParent( '..' )
 
     # Third row ####################
     cmds.rowLayout( adj=2, numberOfColumns=3 )
-    cmds.text( label='second' )
+    cmds.text( label='last' )
     insertionParentField = cmds.textField( pht='Insertion Parent Joint', ed=True )
-    cmds.button( label='Select', command=functools.partial( get_selection, insertionParentField ) )
+    cmds.button( label='Select', command=functools.partial( get_selection, insertionParentField, 'joint' ) )
     
     cmds.setParent( '..' )
     
     # Fourth row ####################
     cmds.button( label='Generate', command=functools.partial( create_stretchy_system, 
-                                                              'test' ) ) #change this after create_stretchy_system edits
+                                                              originParentField, insertionParentField ) ) #change this after create_stretchy_system edits
     #cmds.button( label='Help', command=functools.partial( display_Help_UI ) )
 
         # NEW JOINT CHAIN ---------------------------------------------
@@ -121,114 +121,125 @@ def delete_stretchy_system( pStretchyGroup ):
 
 def create_stretchy_system( pTextField1, pTextField2, *pArgs ):
 
-    firstJoint = cmds.textField( pTextField, query=True, text=True )
-    lastJoint = cmds.textField( pTextField, query=True, text=True )
+    firstJoint = cmds.textField( pTextField1, query=True, text=True )
+    lastJoint = cmds.textField( pTextField2, query=True, text=True )
+    
+    pJointList = []
+    
+    cmds.select( firstJoint, hi=True )
+    pJointList = cmds.ls( sl=True, type='joint' )
+    print( pJointList )
 
-    pJointList = [] # find every joint btwn
-
-    if (len(pJointList) >= 3):
-        #if the grp already exists, delete it
-        if cmds.objExists( 'stretchy_grp' ):
-            cmds.delete( stretchy_grp )
-            
-        #CREATE CONTROLLERS
-        controlCurveList = []
-            
-        for jnt in pJointList:
-            ## Joint Information
-            # Get parent of current joint
-            jntParent = cmds.listRelatives( jnt, parent=True )
-            if( jntParent ):
-                jntParentName = jntParent[0]
-            else:
-                jntParentName = "GARBAGE" #this is for the first joint in the heirarchy since it has no parent. there needs to be something
-                
-            # Get Radius of current joint
-            jntRadius = cmds.getAttr( jnt + '.radius' )
-            
-            # Get translation and rotation of current joint
-            jntTranslation = cmds.xform( jnt, query=True, translation=True, worldSpace=True )
-            jntRotation = cmds.xform( jnt, query=True, rotation=True, worldSpace=True )
-            
-            ## Controller Manipulation
-            # Create new controller
-            newControl = cmds.circle( c=(0, 0, 0), name=(jnt + '_ctrl') ) #c changes the shape of the cirlce, 0,0,0 ensures that it's a full circle
-            controlCurveList.append( newControl[0] ) #newControl will return a dictionary containing the transform and the shape node, you just want the name, hence [0]
-            cmds.move( jntTranslation[0], jntTranslation[1], jntTranslation[2], newControl )
-            cmds.rotate( 90, 0, 90, newControl )
-            cmds.scale( ( 1*jntRadius ), ( 1*jntRadius ), ( 1*jntRadius ), newControl )
-            cmds.makeIdentity( newControl, apply=True )
-            
-            # Create the offset group for our new controller
-            newGroup = cmds.group( empty=True, name=(jnt + '_offset') )
-            cmds.move( jntTranslation[0], jntTranslation[1], jntTranslation[2], newGroup ) #the group and the cntrl are at the same place
-            cmds.makeIdentity( newGroup, apply=True ) #freeze transformations
-            
-            # Make new control a child of new group
-            cmds.parent( newControl, newGroup )
-            
-            # Rotate offset group to match the rotations of the joint
-            cmds.rotate( jntRotation[0], jntRotation[1], jntRotation[2], newGroup )
-            
-            '''
-            ## Place group in the right hierarchy
-            if ( jntParentName + '_ctrl' ) in controlCurveList:
-                cmds.parent( newGroup, (jntParentName + '_ctrl') )
-                print( newGroup + ' parented under ' + jntParentName + '_ctrl' )
-            else:
-                print( 'No parent found' )
-                print( jntParentName ) 
-                print( controlCurveList )
-            '''
-                
-            ## Parent constrain joint under control
-            cmds.parentConstraint( newControl, jnt )
-            
-        #PARENT CONSTRAIN THE MIDDLE CONTROLLER OFFSET GROUP TO THE START AND END CONTROLLERS
-        cmds.parentConstraint( controlCurveList[0], controlCurveList[-1], pJointList[1]+'_offset' )
+    if not (len(pJointList) >= 3): #if there arent at least 3 joints in the pre existing group
+        cmds.error("not enough joints in chain (at least 3 required)", n=True)
+        return 0
         
-        #BUILD HELPER JOINTS
-        jointRotList = []
-        jointTransList = []
+    #if the grp already exists, delete it
+    if cmds.objExists( 'stretchy_grp' ):
+        cmds.delete( 'stretchy_grp' )
+        
+    #CREATE CONTROLLERS
+    controlCurveList = []
+        
+    for jnt in pJointList:
+        ## Joint Information
+        # Get parent of current joint
+        jntParent = cmds.listRelatives( jnt, parent=True )
+        if( jntParent ):
+            jntParentName = jntParent[0]
+        else:
+            jntParentName = "GARBAGE" #this is for the first joint in the heirarchy since it has no parent. there needs to be something
             
-        jointRotList.append(cmds.xform( pJointList[0], rotation = True, worldSpace = True, q = True, )) #the first elemeent in the joint list
-        jointRotList.append(cmds.xform( pJointList[-1], rotation = True, worldSpace = True, q = True, )) #the first elemeent in the joint list
-        jointTransList.append(cmds.xform( pJointList[0], translation = True, worldSpace = True, q = True, )) #the last element in the joint list
-        jointTransList.append(cmds.xform( pJointList[-1], translation = True, worldSpace = True, q = True, )) #the last element in the joint list
+        # Get Radius of current joint
+        jntRadius = cmds.getAttr( jnt + '.radius' )
         
-        helperJoints = []
+        # Get translation and rotation of current joint
+        jntTranslation = cmds.xform( jnt, query=True, translation=True, worldSpace=True )
+        jntRotation = cmds.xform( jnt, query=True, rotation=True, worldSpace=True )
         
-        cmds.select( deselect = True )
-        helperJoints.append( cmds.joint( position = jointTransList[0], orientation = jointRotList[0], name = pJointList[0]+'_helper' ) )
-        cmds.select( deselect = True )
-        helperJoints.append( cmds.joint( position = jointTransList[-1], orientation = jointRotList[-1], name = pJointList[-1]+'_helper' ) )
+        ## Controller Manipulation
+        # Create new controller
+        newControl = cmds.spaceLocator( name=(jnt + '_ctrl') )
+        controlCurveList.append( newControl[0] ) #newControl will return a dictionary containing the transform and the shape node, you just want the name, hence [0]
+        cmds.move( jntTranslation[0], jntTranslation[1], jntTranslation[2], newControl )
+        cmds.makeIdentity( newControl, apply=True )
         
-        cmds.parent( helperJoints[-1], helperJoints[0] ) #parent the current joint to the previous joint
+        # Create the offset group for our new controller
+        newGroup = cmds.group( empty=True, name=(jnt + '_offset') )
+        cmds.move( jntTranslation[0], jntTranslation[1], jntTranslation[2], newGroup ) #the group and the cntrl are at the same place
+        cmds.makeIdentity( newGroup, apply=True ) #freeze transformations
         
-        #parent constrain the helper joints to the controllers
-        cmds.parentConstraint( controlCurveList[0], helperJoints[0] )
-        cmds.parentConstraint( controlCurveList[-1], helperJoints[-1] )
+        # Make new control a child of new group
+        cmds.parent( newControl, newGroup )
         
-        #CREATE THE SCALE EXPRESSIONS
+        # Rotate offset group to match the rotations of the joint
+        cmds.rotate( jntRotation[0], jntRotation[1], jntRotation[2], newGroup )
         
-        middleOffCtrl = pJointList[1]+'_offset'
-        statement = "$peak / pow(1 + $blend * $dist, 2)"
-        
-        cmds.expression( object = middleOffCtrl, string = f" float $dist = abs({helperJoints[-1]}.translateX) + abs({helperJoints[-1]}.translateY) + abs({helperJoints[-1]}.translateZ); float $peak = 6; float $blend = .6; {middleOffCtrl}.scaleX = {statement}; {middleOffCtrl}.scaleY = {statement}; {middleOffCtrl}.scaleZ = {statement};" )
-        
-        #scale constraint the middle joint to the controller
-        cmds.scaleConstraint( pJointList[1]+"_ctrl", pJointList[1], mo = True)
-        
-        #GROUP EVERYTHING INTO stretchy_grp
-        #create group
-        stretchy_grp = cmds.group( f"{pJointList[0]}_helper" , n='stretchy_grp' )
-        
-        #add the controllers into it
-        for jnt in pJointList:
-            cmds.parent( jnt + '_offset', stretchy_grp )
+        '''
+        ## Place group in the right hierarchy
+        if ( jntParentName + '_ctrl' ) in controlCurveList:
+            cmds.parent( newGroup, (jntParentName + '_ctrl') )
+            print( newGroup + ' parented under ' + jntParentName + '_ctrl' )
+        else:
+            print( 'No parent found' )
+            print( jntParentName ) 
+            print( controlCurveList )
+        '''
             
-    else:
-        print("not enough joints selected (3 required)")
+        ## Parent constrain joint under control
+        cmds.parentConstraint( newControl, jnt )
+    
+    '''    
+    #PARENT CONSTRAIN THE MIDDLE CONTROLLER OFFSET GROUP TO THE START AND END CONTROLLERS
+    cmds.parentConstraint( controlCurveList[0], controlCurveList[-1], pJointList[1]+'_offset' )
+    '''
+    
+    #PARENT CONSTRAIN THE MIDDLE CONTROLLER OFFSET GROUPS TO THEIR ADJACENT CONTROLLERS
+    for x in range( len( pJointList ) ) :
+        if pJointList[x] == firstJoint or pJointList[x] == lastJoint:
+            continue
+            
+        cmds.parentConstraint( controlCurveList[x-1], controlCurveList[x+1], pJointList[x]+'_offset' )
+    
+    #BUILD HELPER JOINTS
+    jointRotList = []
+    jointTransList = []
+        
+    jointRotList.append(cmds.xform( pJointList[0], rotation = True, worldSpace = True, q = True, )) #the first elemeent in the joint list
+    jointRotList.append(cmds.xform( pJointList[-1], rotation = True, worldSpace = True, q = True, )) #the first elemeent in the joint list
+    jointTransList.append(cmds.xform( pJointList[0], translation = True, worldSpace = True, q = True, )) #the last element in the joint list
+    jointTransList.append(cmds.xform( pJointList[-1], translation = True, worldSpace = True, q = True, )) #the last element in the joint list
+    
+    helperJoints = []
+    
+    cmds.select( deselect = True )
+    helperJoints.append( cmds.joint( position = jointTransList[0], orientation = jointRotList[0], name = pJointList[0]+'_helper' ) )
+    cmds.select( deselect = True )
+    helperJoints.append( cmds.joint( position = jointTransList[-1], orientation = jointRotList[-1], name = pJointList[-1]+'_helper' ) )
+    
+    cmds.parent( helperJoints[-1], helperJoints[0] ) #parent the current joint to the previous joint
+    
+    #parent constrain the helper joints to the controllers
+    cmds.parentConstraint( controlCurveList[0], helperJoints[0] )
+    cmds.parentConstraint( controlCurveList[-1], helperJoints[-1] )
+    
+    #CREATE THE SCALE EXPRESSIONS
+    
+    middleOffCtrl = pJointList[1]+'_offset'
+    statement = "$peak / pow(1 + $blend * $dist, 2)"
+    
+    cmds.expression( object = middleOffCtrl, string = f" float $dist = abs({helperJoints[-1]}.translateX) + abs({helperJoints[-1]}.translateY) + abs({helperJoints[-1]}.translateZ); float $peak = 6; float $blend = .6; {middleOffCtrl}.scaleX = {statement}; {middleOffCtrl}.scaleY = {statement}; {middleOffCtrl}.scaleZ = {statement};" )
+    
+    #scale constraint the middle joint to the controller
+    cmds.scaleConstraint( pJointList[1]+"_ctrl", pJointList[1], mo = True)
+    
+    #GROUP EVERYTHING INTO stretchy_grp
+    #create group
+    stretchy_grp = cmds.group( f"{pJointList[0]}_helper" , n='stretchy_grp' )
+    
+    #add the controllers into it
+    for jnt in pJointList:
+        cmds.parent( jnt + '_offset', stretchy_grp ) 
 
 ####################################################################################################################################
 
