a23e20735e39504ad13dfe9e9076127b394babe2
Simon <burdicksimon@gmail.com>
2024-09-19 21:02:07 -0700

    New Widget Structure

---
+++ CurveLibraryAdv/CurveLibraryAdv_Tool.py
@@ -0,0 +1,5 @@
+from Widgets import CurveLibraryAdv_Widget
+
+if __name__ == "__main__":
+    CurveLibraryAdvWindow = CurveLibraryAdv_Widget.CurveLibraryAdvDialog()
+    CurveLibraryAdvWindow.GuiShow()
