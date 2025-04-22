a1e579054036bd8cd362bc28ab907cd01eafb179
Simon <burdicksimon@gmail.com>
2025-04-21 21:41:32 -0700

    Deleting old folders

--- CurveLibraryAdv/CurveLibraryAdv_Tool.py
+++
@@ -1,5 +0,0 @@
-from Widgets import CurveLibraryAdv_Widget
-
-if __name__ == "__main__":
-    CurveLibraryAdvWindow = CurveLibraryAdv_Widget.CurveLibraryAdvDialog()
-    CurveLibraryAdvWindow.GuiShow()
