Updated with dedicated Rotation, Translation, and Scale outputs to fix Rotation out issue

SimonsSandbox/SoxsRBF.py
@@ -21,16 +21,29 @@ class SoxsRBFNode(om.MPxNode):
     in_y_obj = None
     in_z_obj = None
 
-    output_obj = None
-    out_x_obj = None
-    out_y_obj = None
-    out_z_obj = None
+    output_t_obj = None
+    out_x_t_obj = None
+    out_y_t_obj = None
+    out_z_t_obj = None
+
+    output_r_obj = None
+    out_x_r_obj = None
+    out_y_r_obj = None
+    out_z_r_obj = None
+
+    output_s_obj = None
+    out_x_s_obj = None
+    out_y_s_obj = None
+    out_z_s_obj = None
 
     rbf_data_path_obj = None
     rbf_id_obj = None
 
     eval_settings_obj = None
     kernel_obj = None
+
+    outputs = []
+    old_position = [0, 0, 0]
 
     SETTINGS_ENUMS = [
         "1In -> 1Out",
@@ -51,92 +64,122 @@ class SoxsRBFNode(om.MPxNode):
 
     def compute(self, plug, data):
 
-        if (
-            plug == SoxsRBFNode.output_obj
-            or plug == SoxsRBFNode.out_x_obj
-            or plug == SoxsRBFNode.out_y_obj
-            or plug == SoxsRBFNode.out_z_obj
-        ):
+        # Get Attributes
+        input_handle = data.inputValue(SoxsRBFNode.input_obj)
+        in_x = input_handle.child(SoxsRBFNode.in_x_obj).asDouble()
+        in_y = input_handle.child(SoxsRBFNode.in_y_obj).asDouble()
+        in_z = input_handle.child(SoxsRBFNode.in_z_obj).asDouble()
+        position = [in_x, in_y, in_z]
 
-            postion = None
-            in_matrix = None
-            weight_matrix = None
-            output = None
+        # check if the new position still matches the old, if it does, no need to recalculate
+        if position == self.old_position:
+            for obj in self.outputs:
+                data.setClean(obj)
 
-            # Get Attributes
-            input_handle = data.inputValue(SoxsRBFNode.input_obj)
-            in_x = input_handle.child(SoxsRBFNode.in_x_obj).asDouble()
-            in_y = input_handle.child(SoxsRBFNode.in_y_obj).asDouble()
-            in_z = input_handle.child(SoxsRBFNode.in_z_obj).asDouble()
+            return
+
+        # Run the calculation code:
+        postion = None
+        in_matrix = None
+        weight_matrix = None
+        output = None
+
+        # Set the old position, so if it doesn't change, no need to recalculate!
+        self.old_position = position
+
+        in_matrix_path = data.inputValue(SoxsRBFNode.rbf_data_path_obj).asString()
+        rbf_id = data.inputValue(SoxsRBFNode.rbf_id_obj).asString()
+
+        eval_index = data.inputValue(SoxsRBFNode.eval_settings_obj).asShort()
+        eq_index = data.inputValue(SoxsRBFNode.kernel_obj).asShort()
+
+        # Read position matrix and weight matrix form JSON file
+        try:
+            with open(in_matrix_path, "r") as f:
+                rbf_data = json.load(f)
+
+                dist_key = f"d_{rbf_id}"
+                weight_key = f"w_{rbf_id}"
+
+                if dist_key not in rbf_data:
+                    om.MGlobal.displayError(
+                        f"Failed to find Distance Matrix for {rbf_id}. Key should be labeled as {dist_key}!"
+                    )
+                    return
+                if weight_key not in rbf_data:
+                    om.MGlobal.displayError(
+                        f"Failed to find Weight Matrix for {rbf_id}. Key should be labeled as {weight_key}!"
+                    )
+                    return
+
+                in_matrix = np.array(rbf_data[dist_key])
+                weight_matrix = np.array(rbf_data[weight_key])
+
+        except Exception as e:
+            om.MGlobal.displayError(f"Failed to read JSON: {e}")
+            return
+
+        # Switch statement for Evaluation Settings
+        if SoxsRBFNode.SETTINGS_ENUMS[eval_index][0:3] == "1In":
+            position = [in_x]
+        elif SoxsRBFNode.SETTINGS_ENUMS[eval_index][0:3] == "2In":
+            position = [in_x, in_y]
+        elif SoxsRBFNode.SETTINGS_ENUMS[eval_index][0:3] == "3In":
             position = [in_x, in_y, in_z]
 
-            in_matrix_path = data.inputValue(SoxsRBFNode.rbf_data_path_obj).asString()
-            rbf_id = data.inputValue(SoxsRBFNode.rbf_id_obj).asString()
+        # DEBUG : delete me
+        # print(in_matrix)
+        # print(weight_matrix)
 
-            eval_index = data.inputValue(SoxsRBFNode.eval_settings_obj).asShort()
-            eq_index = data.inputValue(SoxsRBFNode.kernel_obj).asShort()
+        # Calculate RBF
+        solve_result = self.evaluate_rbf(position, in_matrix, weight_matrix, eq_index)
+        print(solve_result)
 
-            # Read position matrix and weight matrix form JSON file
-            try:
-                with open(in_matrix_path, "r") as f:
-                    rbf_data = json.load(f)
+        # Set outputs
+        output_handle = data.outputValue(SoxsRBFNode.output_t_obj)
+        out_x_t = output_handle.child(SoxsRBFNode.out_x_t_obj)
+        out_y_t = output_handle.child(SoxsRBFNode.out_y_t_obj)
+        out_z_t = output_handle.child(SoxsRBFNode.out_z_t_obj)
 
-                    dist_key = f"d_{rbf_id}"
-                    weight_key = f"w_{rbf_id}"
+        output_handle = data.outputValue(SoxsRBFNode.output_r_obj)
+        out_x_r = output_handle.child(SoxsRBFNode.out_x_r_obj)
+        out_y_r = output_handle.child(SoxsRBFNode.out_y_r_obj)
+        out_z_r = output_handle.child(SoxsRBFNode.out_z_r_obj)
 
-                    if dist_key not in rbf_data:
-                        om.MGlobal.displayError(
-                            f"Failed to find Distance Matrix for {rbf_id}. Key should be labeled as {dist_key}!"
-                        )
-                        return
-                    if weight_key not in rbf_data:
-                        om.MGlobal.displayError(
-                            f"Failed to find Weight Matrix for {rbf_id}. Key should be labeled as {weight_key}!"
-                        )
-                        return
+        output_handle = data.outputValue(SoxsRBFNode.output_s_obj)
+        out_x_s = output_handle.child(SoxsRBFNode.out_x_s_obj)
+        out_y_s = output_handle.child(SoxsRBFNode.out_y_s_obj)
+        out_z_s = output_handle.child(SoxsRBFNode.out_z_s_obj)
 
-                    in_matrix = np.array(rbf_data[dist_key])
-                    weight_matrix = np.array(rbf_data[weight_key])
+        # Set out attributes
+        if SoxsRBFNode.SETTINGS_ENUMS[eval_index][-4:] == "1Out":
+            out_x_t.setDouble(solve_result[0])
+            out_x_r.setMAngle(om.MAngle(solve_result[0], om.MAngle.kDegrees))
+            out_x_s.setDouble(solve_result[0])
 
-            except Exception as e:
-                om.MGlobal.displayError(f"Failed to read JSON: {e}")
-                return
+        if SoxsRBFNode.SETTINGS_ENUMS[eval_index][-4:] == "2Out":
+            out_x_t.setDouble(solve_result[0])
+            out_x_r.setMAngle(om.MAngle(solve_result[0], om.MAngle.kDegrees))
+            out_x_s.setDouble(solve_result[0])
+            out_y_t.setDouble(solve_result[1])
+            out_y_r.setMAngle(om.MAngle(solve_result[1], om.MAngle.kDegrees))
+            out_y_s.setDouble(solve_result[1])
 
-            # Switch statement for Evaluation Settings
-            if SoxsRBFNode.SETTINGS_ENUMS[eval_index][0:3] == "1In":
-                position = [in_x]
-            elif SoxsRBFNode.SETTINGS_ENUMS[eval_index][0:3] == "2In":
-                position = [in_x, in_y]
-            elif SoxsRBFNode.SETTINGS_ENUMS[eval_index][0:3] == "3In":
-                position = [in_x, in_y, in_z]
+        if SoxsRBFNode.SETTINGS_ENUMS[eval_index][-4:] == "3Out":
+            out_x_t.setDouble(solve_result[0])
+            out_x_r.setMAngle(om.MAngle(solve_result[0], om.MAngle.kDegrees))
+            out_x_s.setDouble(solve_result[0])
+            out_y_t.setDouble(solve_result[1])
+            out_y_r.setMAngle(om.MAngle(solve_result[1], om.MAngle.kDegrees))
+            out_y_s.setDouble(solve_result[1])
+            out_z_t.setDouble(solve_result[2])
+            out_z_r.setMAngle(om.MAngle(solve_result[2], om.MAngle.kDegrees))
+            out_z_s.setDouble(solve_result[2])
 
-            output_handle = data.outputValue(SoxsRBFNode.output_obj)
-            out_x = output_handle.child(SoxsRBFNode.out_x_obj)
-            out_y = output_handle.child(SoxsRBFNode.out_y_obj)
-            out_z = output_handle.child(SoxsRBFNode.out_z_obj)
+        for obj in self.outputs:
+            data.setClean(obj)
 
-            # DEBUG : delete me
-            # print(in_matrix)
-            # print(weight_matrix)
-
-            # Calculate RBF
-            solve_result = self.evaluate_rbf(
-                position, in_matrix, weight_matrix, eq_index
-            )
-            print(solve_result)
-
-            # Set out attributes
-            if SoxsRBFNode.SETTINGS_ENUMS[eval_index][-4:] == "1Out":
-                out_x.setDouble(solve_result[0])
-            if SoxsRBFNode.SETTINGS_ENUMS[eval_index][-4:] == "2Out":
-                out_x.setDouble(solve_result[0])
-                out_y.setDouble(solve_result[1])
-            if SoxsRBFNode.SETTINGS_ENUMS[eval_index][-4:] == "3Out":
-                out_x.setDouble(solve_result[0])
-                out_y.setDouble(solve_result[1])
-                out_z.setDouble(solve_result[2])
-
-            data.setClean(plug)
+        return
 
     # RBF Solver
     def evaluate_rbf(self, pos, in_matrix, weight_matrix, beta):
@@ -196,21 +239,81 @@ class SoxsRBFNode(om.MPxNode):
         cAttr.addChild(cls.in_z_obj)
 
         # OUTPUT - Output Attributes
-        cls.out_x_obj = nAttr.create("OutputX", "outX", om.MFnNumericData.kDouble, 0.0)
-        nAttr.writable = False
-        nAttr.storable = False
-        cls.out_y_obj = nAttr.create("OutputY", "outY", om.MFnNumericData.kDouble, 0.0)
-        nAttr.writable = False
-        nAttr.storable = False
-        cls.out_z_obj = nAttr.create("OutputZ", "outZ", om.MFnNumericData.kDouble, 0.0)
+
+        # Translate
+        cls.out_x_t_obj = nAttr.create(
+            "OutputTranslateX", "outTX", om.MFnNumericData.kDouble, 0.0
+        )
         nAttr.writable = False
         nAttr.storable = False
 
-        cls.output_obj = cAttr.create("Output", "out")
+        cls.out_y_t_obj = nAttr.create(
+            "OutputTranslateY", "outTY", om.MFnNumericData.kDouble, 0.0
+        )
+        nAttr.writable = False
+        nAttr.storable = False
+
+        cls.out_z_t_obj = nAttr.create(
+            "OutputTranslateZ", "outTZ", om.MFnNumericData.kDouble, 0.0
+        )
+        nAttr.writable = False
+        nAttr.storable = False
+
+        cls.output_t_obj = cAttr.create("OutputTranslate", "outT")
         cAttr.writable = False
-        cAttr.addChild(cls.out_x_obj)
-        cAttr.addChild(cls.out_y_obj)
-        cAttr.addChild(cls.out_z_obj)
+        cAttr.addChild(cls.out_x_t_obj)
+        cAttr.addChild(cls.out_y_t_obj)
+        cAttr.addChild(cls.out_z_t_obj)
+
+        # Rotate
+        cls.out_x_r_obj = uAttr.create(
+            "OutputRotateX", "outRX", om.MFnUnitAttribute.kAngle, 0.0
+        )
+        nAttr.writable = False
+        nAttr.storable = False
+
+        cls.out_y_r_obj = uAttr.create(
+            "OutputRotateY", "outRY", om.MFnUnitAttribute.kAngle, 0.0
+        )
+        nAttr.writable = False
+        nAttr.storable = False
+
+        cls.out_z_r_obj = uAttr.create(
+            "OutputRotateZ", "outRZ", om.MFnUnitAttribute.kAngle, 0.0
+        )
+        nAttr.writable = False
+        nAttr.storable = False
+
+        cls.output_r_obj = cAttr.create("OutputRotate", "outR")
+        cAttr.writable = False
+        cAttr.addChild(cls.out_x_r_obj)
+        cAttr.addChild(cls.out_y_r_obj)
+        cAttr.addChild(cls.out_z_r_obj)
+
+        # Scale
+        cls.out_x_s_obj = nAttr.create(
+            "OutputScaleX", "outSX", om.MFnNumericData.kDouble, 0.0
+        )
+        nAttr.writable = False
+        nAttr.storable = False
+
+        cls.out_y_s_obj = nAttr.create(
+            "OutputScaleY", "outSY", om.MFnNumericData.kDouble, 0.0
+        )
+        nAttr.writable = False
+        nAttr.storable = False
+
+        cls.out_z_s_obj = nAttr.create(
+            "OutputScaleZ", "outSZ", om.MFnNumericData.kDouble, 0.0
+        )
+        nAttr.writable = False
+        nAttr.storable = False
+
+        cls.output_s_obj = cAttr.create("OutputScale", "outS")
+        cAttr.writable = False
+        cAttr.addChild(cls.out_x_s_obj)
+        cAttr.addChild(cls.out_y_s_obj)
+        cAttr.addChild(cls.out_z_s_obj)
 
         # INPUT - Json Path
         cls.rbf_data_path_obj = tAttr.create(
@@ -226,7 +329,9 @@ class SoxsRBFNode(om.MPxNode):
 
         # Add all the attributes
         cls.addAttribute(cls.input_obj)
-        cls.addAttribute(cls.output_obj)
+        cls.addAttribute(cls.output_t_obj)
+        cls.addAttribute(cls.output_r_obj)
+        cls.addAttribute(cls.output_s_obj)
 
         cls.addAttribute(cls.rbf_data_path_obj)
         cls.addAttribute(cls.rbf_id_obj)
@@ -234,9 +339,21 @@ class SoxsRBFNode(om.MPxNode):
         cls.addAttribute(cls.eval_settings_obj)
 
         # Add attribute affects
-        all_outputs = [cls.output_obj, cls.out_x_obj, cls.out_y_obj, cls.out_z_obj]
-
-        for obj in all_outputs:
+        cls.outputs = [
+            cls.output_t_obj,
+            cls.out_x_t_obj,
+            cls.out_y_t_obj,
+            cls.out_z_t_obj,
+            cls.output_r_obj,
+            cls.out_x_r_obj,
+            cls.out_y_r_obj,
+            cls.out_z_r_obj,
+            cls.output_s_obj,
+            cls.out_x_s_obj,
+            cls.out_y_s_obj,
+            cls.out_z_s_obj,
+        ]
+        for obj in cls.outputs:
             cls.attributeAffects(cls.in_x_obj, obj)
             cls.attributeAffects(cls.in_y_obj, obj)
             cls.attributeAffects(cls.in_z_obj, obj)